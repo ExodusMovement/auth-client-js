2d04ae91313794c3b46bf32ce2e0e3ba73ff5b06
Matias Lang <matias.la@exodus.io>
2023-06-05 17:33:18 -0300

    auth-client: do some hardenning, remove maps and bump version

--- packages/auth-client/src/constants/engine.ts
+++ packages/auth-client/src/constants/engine.ts
@@ -1,20 +1,23 @@
 import { ONE_DAY, FIVE_MINUTES, SEVEN_DAYS } from "@exodus/walletconnect-time";
 import { JsonRpcTypes, RpcOpts } from "../types";
 
-export const ENGINE_RPC_OPTS: Record<JsonRpcTypes.WcMethod, RpcOpts> = {
-  wc_authRequest: {
-    req: {
-      ttl: ONE_DAY,
-      prompt: true,
-      tag: 3000,
-    },
-    res: {
-      ttl: ONE_DAY,
-      prompt: false,
-      tag: 3001,
+export const ENGINE_RPC_OPTS: Record<JsonRpcTypes.WcMethod, RpcOpts> = Object.assign(
+  Object.create(null),
+  {
+    wc_authRequest: {
+      req: {
+        ttl: ONE_DAY,
+        prompt: true,
+        tag: 3000,
+      },
+      res: {
+        ttl: ONE_DAY,
+        prompt: false,
+        tag: 3001,
+      },
     },
   },
-};
+);
 
 export const AUTH_REQUEST_EXPIRY_BOUNDARIES = {
   min: FIVE_MINUTES,
--- packages/auth-client/src/utils/signature.ts
+++ packages/auth-client/src/utils/signature.ts
@@ -1,9 +1,7 @@
 import { hashMessage } from "@exodus/ethersproject-hash";
 import { recoverAddress } from "@exodus/ethersproject-transactions";
-import fetch from "isomorphic-unfetch";
 
 import { AuthEngineTypes } from "../types";
-import { DEFAULT_RPC_URL } from "../constants/defaults";
 
 export async function verifySignature(
   address: string,
@@ -37,48 +35,11 @@ function isValidEip191Signature(address: string, message: string, signature: str
 }
 
 async function isValidEip1271Signature(
-  address: string,
-  reconstructedMessage: string,
-  signature: string,
-  chainId: string,
-  projectId: string,
-) {
-  try {
-    const eip1271MagicValue = "0x1626ba7e";
-    const dynamicTypeOffset = "0000000000000000000000000000000000000000000000000000000000000040";
-    const dynamicTypeLength = "0000000000000000000000000000000000000000000000000000000000000041";
-    const nonPrefixedSignature = signature.substring(2);
-    const nonPrefixedHashedMessage = hashMessage(reconstructedMessage).substring(2);
-
-    const data =
-      eip1271MagicValue +
-      nonPrefixedHashedMessage +
-      dynamicTypeOffset +
-      dynamicTypeLength +
-      nonPrefixedSignature;
-
-    const response = await fetch(`${DEFAULT_RPC_URL}/?chainId=${chainId}&projectId=${projectId}`, {
-      method: "POST",
-      body: JSON.stringify({
-        id: generateJsonRpcId(),
-        jsonrpc: "2.0",
-        method: "eth_call",
-        params: [{ to: address, data }, "latest"],
-      }),
-    });
-    const { result } = await response.json();
-
-    if (!result) return false;
-
-    // Remove right-padded zeros from result to get only the concrete recovered value.
-    const recoveredValue = result.slice(0, eip1271MagicValue.length);
-    return recoveredValue.toLowerCase() === eip1271MagicValue.toLowerCase();
-  } catch (error: any) {
-    console.error("isValidEip1271Signature: ", error);
-    return false;
-  }
-}
-
-function generateJsonRpcId() {
-  return Date.now() + Math.floor(Math.random() * 1000);
+  _address: string,
+  _reconstructedMessage: string,
+  _signature: string,
+  _chainId: string,
+  _projectId: string,
+): Promise<boolean> {
+  throw new Error("isValidEip1271Signature disabled in Exodus fork");
 }
