1614f2766d86fd98c2ad7604bcc173add3791b02
Matias Lang <matias.la@exodus.io>
2023-06-05 14:27:32 -0300

    remove uint8arrays dependency and bump version

--- packages/auth-client/src/utils/crypto.ts
+++ packages/auth-client/src/utils/crypto.ts
@@ -1,5 +1,4 @@
 import SHA from "sha.js";
-import { fromString, toString } from "uint8arrays";
 
 export const BASE10 = "base10";
 export const BASE16 = "base16";
@@ -7,6 +6,5 @@ export const BASE64 = "base64pad";
 export const UTF8 = "utf8";
 
 export function hashMessage(message: string): string {
-  const result = SHA("sha256").update(fromString(message, UTF8)).digest();
-  return toString(result, BASE16);
+  return SHA("sha256").update(Buffer.from(message)).digest("hex");
 }
